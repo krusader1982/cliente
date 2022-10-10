14d089ef46956020f4e0be511f2be49357807b47
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2022-10-10 04:15:07 +0000

    css

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -4,21 +4,24 @@ import { CgProfile } from 'react-icons/cg';
 import "./Pagination.css"
 
 const Pagination = () => {
-  return (
-    <div className="body">
-        <div className='image-container'>
-            <h1><CgProfile /></h1>
-            <h1><CgProfile /></h1>
+    return (
+        <div>
+            <div className="body">
+                <div className='image-container'>
+                    <h1><CgProfile /></h1>
+                    <h1><CgProfile /></h1>
+                </div>
+                <div className='image-container'>
+                    <h1><CgProfile /></h1>
+                    <h1><CgProfile /></h1>
+                </div>
+            </div>
             <div>
-            <h1 className='link'>https://www.youtube.com/watch?v=voXTVTW73E8</h1>
+                <h1 className='link'>https://www.youtube.com/watch?v=voXTVTW73E8</h1>
             </div>
         </div>
-        <div className='image-container'>
-            <h1><CgProfile /></h1>
-            <h1><CgProfile /></h1>
-        </div>            
-    </div>
-  )
+
+    )
 }
 
 export default Pagination
