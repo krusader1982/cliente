7f91d60003dc59a2de38a614f61ca70922fc2547
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2022-10-18 06:09:25 +0000

    css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Tabela from './components/Tabela';
-import TabelaFilter from './components/Tabelas/TabelaFilter';
-import Home from './components/Home/Home';
+//import { BrowserRouter, Route, Routes } from 'react-router-dom'
+//import Tabela from './components/Tabela';
+//import TabelaFilter from './components/Tabelas/TabelaFilter';
+//import Home from './components/Home/Home';
 
-import './App.css';
-import Pagination from './components/Pagination/Pagination';
+//import './App.css';
+//import Pagination from './components/Pagination/Pagination';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/tabela_simples" element={<Tabela />} />
-      </Routes>
-      <Routes>
-        <Route path="/tabela_filter" element={<TabelaFilter />} />
-      </Routes>
-      <Routes>
-        <Route path="/pagination" element={<Pagination />} />
-      </Routes>
-    </BrowserRouter>
+    //   <BrowserRouter>
+    //      <Routes>
+    //       <Route path="/" element={<Home />} />
+    //      </Routes>
+    //      <Routes>
+    //        <Route path="/tabela_simples" element={<Tabela />} />
+    //      </Routes>
+    //      <Routes>
+    //        <Route path="/tabela_filter" element={<TabelaFilter />} />
+    //      </Routes>
+    //      <Routes>
+    //        <Route path="/pagination" element={<Pagination />} />
+    //      </Routes>
+    //    </BrowserRouter>
+    <div className='container'>
+      <header></header>
+      <aside> </aside>
+      <main>
+        <div className="container2">
+          <div className="top_r"></div>
+          <div></div>
+          <div className="top_l"></div>
+          <div></div>
+        </div>
+
+      </main>
+      <footer></footer>
+    </div>
+
 
   )
 }
